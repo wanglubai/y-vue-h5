01b4cd113b86c79ffe9b56bcf8ffaed4e8dc4995
王爽 <wangshuang@plu.cn>
2018-08-13 19:27:46 +0800

    add

--- src/modules/y-demo/components/c-mvvm.vue
+++ src/modules/y-demo/components/c-mvvm.vue
@@ -4,18 +4,18 @@
    <p>实现mvvm主要包含两个方面，数据变化更新视图，视图变化更新数据：</p>
    <p class="c-mvvm-mvimg"></p>
    <p>关键点在于data如何更新view，因为view更新data其实可以通过事件监听即可，比如input标签监听 'input' 事件就可以实现了。所以我们着重来分析下，当数据改变，如何更新视图的。</p>
-  <p>数据更新视图的重点是如何知道数据变了，只要知道数据变了，那么接下去的事都好处理。如何知道数据变了，就是通过Object.defineProperty( )对属性设置一个set函数，当数据改变了就会来触发这个函数，所以我们只要将一些需要更新的方法放在这里面就可以实现data更新view了。</p>
+  <p>数据更新视图的重点是如何知道数据变了，只要知道数据变了，那么接下去的事都好处理。如何知道数据变了，就是通过<code>Object.defineProperty( )</code>对属性设置一个set函数，当数据改变了就会来触发这个函数，所以我们只要将一些需要更新的方法放在这里面就可以实现data更新view了。</p>
   <p class="c-mvvm-mvc"></p>
   <h2>实现过程</h2>
-  <p>我们已经知道实现数据的双向绑定，首先要对数据进行劫持监听，所以我们需要设置一个监听器Observer，用来监听所有属性。如果属性发上变化了，就需要告诉订阅者Watcher看是否需要更新。因为订阅者是有很多个，所以我们需要有一个消息订阅器Dep来专门收集这些订阅者，然后在监听器Observer和订阅者Watcher之间进行统一管理的。接着，我们还需要有一个指令解析器Compile，对每个节点元素进行扫描和解析，将相关指令对应初始化成一个订阅者Watcher，并替换模板数据或者绑定相应的函数，此时当订阅者Watcher接收到相应属性的变化，就会执行对应的更新函数，从而更新视图。因此接下去我们执行以下3个步骤，实现数据的双向绑定：</p>
- <p>1.实现一个监听器Observer，用来劫持并监听所有属性，如果有变动的，就通知订阅者。</p>
- <p>2.实现一个订阅者Watcher，可以收到属性的变化通知并执行相应的函数，从而更新视图。</p>
- <p>3.实现一个解析器Compile，可以扫描和解析每个节点的相关指令，并根据初始化模板数据以及初始化相应的订阅器。</p>
+  <p>我们已经知道实现数据的双向绑定，首先要对数据进行劫持监听，所以我们需要设置一个监听器<code>Observer</code>，用来监听所有属性。如果属性发上变化了，就需要告诉订阅者<code>Watcher</code>看是否需要更新。因为订阅者是有很多个，所以我们需要有一个消息订阅器Dep来专门收集这些订阅者，然后在监听器<code>Observer</code>和订阅者<code>Watcher</code>之间进行统一管理的。接着，我们还需要有一个指令解析器Compile，对每个节点元素进行扫描和解析，将相关指令对应初始化成一个订阅者<code>Watcher</code>，并替换模板数据或者绑定相应的函数，此时当订阅者<code>Watcher</code>接收到相应属性的变化，就会执行对应的更新函数，从而更新视图。因此接下去我们执行以下3个步骤，实现数据的双向绑定：</p>
+ <p>1.实现一个监听器<code><code>Observer</code></code>，用来劫持并监听所有属性，如果有变动的，就通知订阅者。</p>
+ <p>2.实现一个订阅者<code><code>Watcher</code></code>，可以收到属性的变化通知并执行相应的函数，从而更新视图。</p>
+ <p>3.实现一个解析器<code>Compile</code>，可以扫描和解析每个节点的相关指令，并根据初始化模板数据以及初始化相应的订阅器。</p>
  <p>流程图如下：</p>
  <p class="c-mvvm-mvvm"></p>
- <h2>1.实现一个Observer</h2>
- <p>Observer是一个数据监听器，其实现核心方法就是前文所说的Object.defineProperty( )。如果要对所有属性都进行监听的话，那么可以通过递归方法遍历所有属性值，并对其进行Object.defineProperty( )处理。如下代码，实现了一个Observer。</p>
- <pre>function defineReactive(data, key, val) {
+ <h2>1.实现一个<code>Observer</code></h2>
+ <p><code>Observer</code>是一个数据监听器，其实现核心方法就是前文所说的<code><code>Object.defineProperty( )</code></code>。如果要对所有属性都进行监听的话，那么可以通过递归方法遍历所有属性值，并对其进行<code>Object.defineProperty( )</code>处理。如下代码，实现了一个<code>Observer</code>。</p>
+ <div class="y-js"><pre>function defineReactive(data, key, val) {
     observe(val); // 递归遍历所有子属性
     var dep = new Dep(); 
     Object.defineProperty(data, key, {
@@ -23,7 +23,7 @@
         configurable: true,
         get: function() {
             if (是否需要添加订阅者) {
-                dep.addSub(watcher); // 在这里添加一个订阅者
+                dep.addSub(<code>Watcher</code>); // 在这里添加一个订阅者
             }
             return val;
         },
@@ -50,18 +50,18 @@ Dep.prototype = {
             sub.update();
         });
     }
-};</pre>
-<p>从代码上看，我们将订阅器Dep添加一个订阅者设计在getter里面，这是为了让Watcher初始化进行触发，因此需要判断是否要添加订阅者，至于具体设计方案，下文会详细说明的。在setter函数里面，如果数据变化，就会去通知所有订阅者，订阅者们就会去执行对应的更新的函数。到此为止，一个比较完整Observer已经实现了，接下来我们开始设计Watcher。</p>
- <h2>2.实现Watcher</h2>
- <p>订阅者Watcher在初始化的时候需要将自己添加进订阅器Dep中，那该如何添加呢？我们已经知道监听器Observer是在get函数执行了添加订阅者Wather的操作的，所以我们只要在订阅者Watcher初始化的时候出发对应的get函数去执行添加订阅者操作即可，那要如何触发get的函数，再简单不过了，只要获取对应的属性值就可以触发了，核心原因就是因为我们使用了Object.defineProperty( )进行数据监听。这里还有一个细节点需要处理，我们只要在订阅者Watcher初始化的时候才需要添加订阅者，所以需要做一个判断操作，因此可以在订阅器上做一下手脚：在Dep.target上缓存下订阅者，添加成功后再将其去掉就可以了。订阅者Watcher的实现如下：</p>
- <pre>function Watcher(vm, exp, cb) {
+};</pre></div>
+<p>从代码上看，我们将订阅器Dep添加一个订阅者设计在getter里面，这是为了让<code><code>Watcher</code></code>初始化进行触发，因此需要判断是否要添加订阅者，至于具体设计方案，下文会详细说明的。在setter函数里面，如果数据变化，就会去通知所有订阅者，订阅者们就会去执行对应的更新的函数。到此为止，一个比较完整<code>Observer</code>已经实现了，接下来我们开始设计<code>Watcher</code>。</p>
+ <h2>2.实现<code>Watcher</code></h2>
+ <p>订阅者<code>Watcher</code>在初始化的时候需要将自己添加进订阅器Dep中，那该如何添加呢？我们已经知道监听器<code>Observer</code>是在get函数执行了添加订阅者Wather的操作的，所以我们只要在订阅者<code>Watcher</code>初始化的时候出发对应的get函数去执行添加订阅者操作即可，那要如何触发get的函数，再简单不过了，只要获取对应的属性值就可以触发了，核心原因就是因为我们使用了<code>Object.defineProperty( )</code>进行数据监听。这里还有一个细节点需要处理，我们只要在订阅者<code>Watcher</code>初始化的时候才需要添加订阅者，所以需要做一个判断操作，因此可以在订阅器上做一下手脚：在Dep.target上缓存下订阅者，添加成功后再将其去掉就可以了。订阅者<code>Watcher</code>的实现如下：</p>
+ <div class="y-js"><pre>function <code>Watcher</code>(vm, exp, cb) {
     this.cb = cb;
     this.vm = vm;
     this.exp = exp;
     this.value = this.get();  // 将自己添加到订阅器的操作
 }
  
-Watcher.prototype = {
+<code>Watcher</code>.prototype = {
     update: function() {
         this.run();
     },
@@ -79,9 +79,9 @@ Watcher.prototype = {
         Dep.target = null;  // 释放自己
         return value;
     }
-};</pre>
-<p>这时候，我们需要对监听器Observer也做个稍微调整，主要是对应Watcher类原型上的get函数。需要调整地方在于defineReactive函数：</p>
-<pre>function defineReactive(data, key, val) {
+};</pre></div>
+<p>这时候，我们需要对监听器<code><code>Observer</code></code>也做个稍微调整，主要是对应<code><code>Watcher</code></code>类原型上的get函数。需要调整地方在于<code>defineReactive</code>函数：</p>
+<div class="y-js"><pre>function defineReactive(data, key, val) {
     observe(val); // 递归遍历所有子属性
     var dep = new Dep(); 
     Object.defineProperty(data, key, {
@@ -103,8 +103,8 @@ Watcher.prototype = {
         }
     });
 }
-Dep.target = null;</pre>
-<p>到此为止，简单版的Watcher设计完毕，这时候我们只要将Observer和Watcher关联起来，就可以实现一个简单的双向绑定数据了。</p>
+Dep.target = null;</pre></div>
+<p>到此为止，简单版的<code><code>Watcher</code></code>设计完毕，这时候我们只要将<code><code>Observer</code></code>和<code><code>Watcher</code></code>关联起来，就可以实现一个简单的双向绑定数据了。</p>
  </div>
 </template>
 <script>
